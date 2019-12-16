1c55fc155e07c5f9fe326c01a96cde2af4228f70
jemprojects <jmprojectsgit@gmail.com>
2019-12-16 13:06:00 -0300

    .

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,22 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavegationComponent } from './components/navegation/navegation.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 import { EntradasFormComponent } from './components/entradas-form/entradas-form.component';
 import { EntradasListComponent } from './components/entradas-list/entradas-list.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import {MatMenuModule} from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NavegationComponent } from './components/navegation/navegation.component';
+import { NgModule } from '@angular/core';
 
+//https://github.com/FaztWeb/angular7-mysql-crud
+//youtube.com/watch?v=lxYB79ANJM8
 @NgModule({
   declarations: [
     AppComponent,
     NavegationComponent,
     EntradasFormComponent,
-    EntradasListComponent
+    EntradasListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatMenuModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
