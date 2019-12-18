134e5581cce6fba4015dc0d75d9e7f4af75b67cb
jemprojects <jmprojectsgit@gmail.com>
2019-12-18 12:31:37 -0300

    Genere interfaz buques y entradas

---
+++ client/src/app/models/buques.ts
@@ -0,0 +1,14 @@
+
+export interface Buques {
+  ORDEN?: number;
+  NOMBRE?: string;
+  BANDERA?: number;
+  ARBOLADURA?: number;
+  ESLORA?: number;
+  MANGA?: number;
+  PUNTAL?: number;
+  TRN?: number;
+  TRB?: number;
+  IMO?: number;
+  Campo1?: number;
+}
---
+++ client/src/app/models/entradas.ts
@@ -0,0 +1,25 @@
+export interface Buques {
+  ID?: number;
+  GIRO?: number
+  BUQUE?: number;
+  AGENCIA?: number;
+  PROCEDENCIA?: number;
+  DESTINO?: number;
+  ENTRADA?: string;
+  SALIDA?: string;
+  MUELLE?: number;
+  TRAFICO?: number;
+  DOCUMENTO?: number;
+  Nº?: number;
+  CAL_ENT?: number:
+  CAL_SAL?: number;
+  ENVASE_DESC?: string;
+  EMPRESAS_DEC?: string;
+  CARGA?: string;
+  TNS_CARG?: string;
+  ENVASE?: string;
+  EMPRESAS_CAR?: string;
+  CAL_ENT1?: string;
+  CAL_SAL1?: string;
+  TIPO?: string;
+}
