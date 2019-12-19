7205646a3fc35c256d9b5561984f5f7ce750848f
jemprojects <jmprojectsgit@gmail.com>
2019-12-19 10:40:19 -0300

    interface buques y entradas

--- client/src/app/models/buques.ts
+++ client/src/app/models/buques.ts
@@ -1,14 +1,13 @@
-
 export interface Buques {
-  ORDEN?: number;
-  NOMBRE?: string;
-  BANDERA?: number;
-  ARBOLADURA?: number;
-  ESLORA?: number;
-  MANGA?: number;
-  PUNTAL?: number;
-  TRN?: number;
-  TRB?: number;
-  IMO?: number;
-  Campo1?: number;
+  ORDEN?: number,
+  NOMBRE?: string,
+  BANDERA?: number,
+  ARBOLADURA?: number,
+  ESLORA?: number,
+  MANGA?: number,
+  PUNTAL?: number,
+  TRN?: number,
+  TRB?: number,
+  IMO?: number,
+  Campo1?: number,
 }
