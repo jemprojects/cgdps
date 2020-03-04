9fa4997ee18bef13d5edda7baf6bfe2daef170e3
jemprojects <jmprojectsgit@gmail.com>
2020-03-04 12:39:12 -0300

    listo

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCdX3tOncHDMINAvNrQHT8WbwiWlO5hWPM",
+    authDomain: "cgpds-6e853.firebaseapp.com",
+    databaseURL: "https://cgpds-6e853.firebaseio.com",
+    projectId: "cgpds-6e853",
+    storageBucket: "cgpds-6e853.appspot.com",
+    messagingSenderId: "315745944644",
+    appId: "1:315745944644:web:56ab0b305616b7a8946811",
+    measurementId: "G-FBLEKW9PGW"
+  }
 };
